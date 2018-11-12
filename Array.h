3dd0a5aaf8cd865575ba090f46674dc04a15be5d
igor317 <44530414+igor317@users.noreply.github.com>
2018-11-12 21:04:02 +0300

    Update Array.h

--- Array.h
+++ Array.h
@@ -1,65 +1,62 @@
 /*
-0 - Пустая клетка
-1 - Коридор прямо
-6 - Поворот против ч.с.
-7 - Поворот по ч.с.
-2 - Вход
-3 - Выход
-8 - Внешняя стена
-9 - Запрещенная клетка (в углах)
+ДОСТУП:
+	0 - Пустая клетка
+	1 - Коридор прямо
+	2 - Вход
+	3 - Выход
+	6 - Поворот против ч.с.
+	7 - Поворот по ч.с.
+	8 - Внешняя стена
+	9 - Запрещенная клетка (в углах)
 */
 #ifndef ARRAY_H
 #define ARRAY_H
 
-struct Position 
+struct Position // Структура позиции
 {
 	int x;
 	int y;
-	int vector;
-	int color;
+	int vector; // направление(для определения направление поворота(угла))
 };
-struct Neigh 
+struct Neighbor // Структура соседей(для проверки клетки на доступность)
 {
 	int x;
 	int y;
-	int access;
+	int access; // доступ
 	int vector;
 };
 
 class Arr
 {
 private:
-	int _x;
-	int _y;
-	int randX_in, randY_in;
-	int randX_out, randY_out;
-	int** mass;
-	int maxtry;
+	int _x;						// Строки
+	int _y;						// Столбцы
+	int randX_in, randY_in;		// позиция входа
+	int randX_out, randY_out;	// позиция выхода
+	int** mass;					// Карта
+	int maxtry;					// Максимальное количество попыток прохода (решение проблемы зацикливания\тупика)
+	int steps;					// Количество шагов
+	int count;					// Количество попыток прохода
+	int _error;					// Ошибки (0 - нет ошибок/1 - зациклился/2 - нашел выход)
+	int minsteps;				// Минимальное кол-во шагов
 
-	Position curpos;
-	Neigh neighb[4];
+	Position curpos;	// Текущая позиция
+	Neighbor neighb[4]; // Соседи
 
-	void Create_arr();
-	void FillMass();
-	void GenIn();
-	void GenOut();
-	void FindNeigh();
-	void Regenerate();
-
-
+	void Create_arr();	// Создание массива
+	void FillMass();	// Заполнение массива(создание шаблона карты)
+	void GenIn();		// Генерация позиции входа
+	void GenOut();		// Генерация позиции выхода
+	void FindNeigh();	// Поиск соседей текущей позиции
 
 public:
-	Arr(int x, int y);
-	~Arr();
-	int WriteArr(int x,int y);
-	void GenWay();
-	Position GetPosition();
-	void test();
-	int tries;
-	int steps;
-	int count;
-	int f;
-
+	Arr(int x, int y, int minsteps);	// Конструктор класса
+	~Arr();								// Деструктор класса
+	int WriteArr(int x,int y);			// Вывод точки массива(карты)
+	int GetSteps();						// Получить количество шагов
+	int GetError();						// Получить ошибку
+	void GenWay();						// Генерация пути (шаг)
+	void SetSteps(int steps);			// Установить количество шагов
 };
 
 #endif
